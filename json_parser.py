8177cb14c3695dd9e029e4ffbd2dc44c98692c65
dmesek <mesek.dominik@gmail.com>
2021-11-02 09:49:26 +0100

    finished

--- json_parser.py
+++ json_parser.py
@@ -7,15 +7,16 @@ VERSION = "version"
 NUMBER_OF_VERSIONS = 20
 
 channel = sys.argv[1]
+version_file = sys.argv[2]
+version_labels_file = sys.argv[3]
 data = sys.stdin.read()
 
 all_versions = json.loads(data)[RELEASES]
 filtered_versions = [v for v in all_versions if v[CHANNEL] == channel]
 filtered_version_names = map(lambda v: v[VERSION], filtered_versions)
 
-f = open('availableVersions', "a")
-f2 = open('availableVersionLabels', "a")
-for i in range(20):
-    current_index = NUMBER_OF_VERSIONS - i
-    f.write('[{index}] {version}\n'.format(index=current_index, version=filtered_version_names[current_index]))
-    f2.write('{version}\n'.format(version=filtered_versions[current_index]['archive']))
+f = open(version_file, "a")
+f2 = open(version_labels_file, "a")
+for i in range(NUMBER_OF_VERSIONS, -1, -1):
+    f.write('[{index}] {version}\n'.format(index=i+1, version=filtered_version_names[i]))
+    f2.write('{version}\n'.format(version=filtered_versions[i]['archive']))
--- version_downloader.sh
+++ version_downloader.sh
@@ -3,22 +3,34 @@
 VERSION_BASE_URL=https://storage.googleapis.com/flutter_infra_release/releases/
 VERSION_JSON_URL="${VERSION_BASE_URL}releases_macos.json"
 
-touch availableVersions
-touch availableVersionLabels
+VERSION_LABEL_FOLDER="/tmp/availableVersionLabels"
+VERSION_FOLDER="/tmp/availableVersions"
 
-curl $VERSION_JSON_URL | python json_parser.py $1
-cat availableVersions
+curl $VERSION_JSON_URL | python json_parser.py $1 $VERSION_FOLDER $VERSION_LABEL_FOLDER
+cat $VERSION_FOLDER
 
 echo "Enter index of desired version: "
 read inputVersion
 
-selectedArchive=`cat availableVersionLabels | tail -n $inputVersion | head -n 1`
+selectedArchive=`cat ${VERSION_LABEL_FOLDER} | tail -n $inputVersion | head -n 1`
 selectedVersion=`echo $selectedArchive | cut -d '_' -f3`
-echo $selectedArchive
+echo "Downloading ${selectedVersion}..."
 
 
-rm availableVersions
-rm availableVersionLabels
+rm $VERSION_FOLDER
+rm $VERSION_LABEL_FOLDER
 
-curl "${VERSION_BASE_URL}${selectedArchive}" -o "$selectedVersion"
+source fvc.config
+DOWNLOADED_FILE_PATH="${flutterVersionFolder}/${selectedVersion}"
+versionLabel=${selectedVersion::${#selectedVersion}-4}
+echo $DOWNLOADED_FILE_PATH
+curl "${VERSION_BASE_URL}${selectedArchive}" -o "$DOWNLOADED_FILE_PATH"
+
+echo "Downloaded ${selectedVersion} into ${flutterVersionFolder}"
+echo "Unzipping ${selectedVersion}. Please wait..."
+`unzip ${DOWNLOADED_FILE_PATH} -d ${flutterVersionFolder} > /dev/null`
+mv "${flutterVersionFolder}/flutter" "${flutterVersionFolder}/${versionLabel}"
+rm "${DOWNLOADED_FILE_PATH}"
+echo "Done."
+
 
